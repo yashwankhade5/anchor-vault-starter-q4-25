896750e38a139ec26e13564419dff6d0bce10d6f
Yash Wankhade <yashwankhade5@gmail.com>
2026-02-01 08:42:26 -0800

    final commit

--- programs/anchor-vault-q4-25/src/lib.rs
+++ programs/anchor-vault-q4-25/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::{
+    error_code,
     prelude::*,
     system_program::{transfer, Transfer},
 };
@@ -18,14 +19,13 @@ pub mod anchor_vault_q4_25 {
     }
 
     pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
-        ctx.accounts.withdraw(amount);
-        Ok(())
+        ctx.accounts.withdraw(amount)
     }
 
-    // pub fn close(ctx: Context<Close>) -> Result<()> {
-    //     // ctx.accounts.close()
-    //     Ok(())
-    // }
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()
+        
+    }
 }
 
 #[derive(Accounts)]
@@ -109,7 +109,6 @@ impl<'info> Deposit<'info> {
 
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
-
     #[account(mut)]
     pub user: Signer<'info>,
     #[account(
@@ -124,44 +123,87 @@ pub struct Withdraw<'info> {
     )]
     pub vault_state: Account<'info, VaultState>,
     pub system_program: Program<'info, System>,
-
-
 }
 
 impl<'info> Withdraw<'info> {
     pub fn withdraw(&mut self, _amount: u64) -> Result<()> {
-       
         let user = self.user.to_account_info();
-        let  vault_state = self.vault_state.key();
+        let vault_state = self.vault_state.key();
+        let vault_amount = self.vault.try_lamports()?;
+        let rent = Rent::get()?.minimum_balance(self.vault.to_account_info().data_len());
+        require!(_amount < vault_amount - rent, Verror::InsufficientSol);
 
         let cpi_program = self.system_program.to_account_info();
-        let signer_seeds :&[&[&[u8]]]    =&[&[b"vault",vault_state.as_ref(),&[self.vault_state.vault_bump]]];
-        
-        let cpi_accounts= Transfer {
-            from:self.vault.to_account_info(),
-            to:self.user.to_account_info(),
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"vault",
+            vault_state.as_ref(),
+            &[self.vault_state.vault_bump],
+        ]];
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
         };
-        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts,signer_seeds);
- transfer(cpi_ctx, _amount)?;
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        transfer(cpi_ctx, _amount)?;
         Ok(())
     }
 }
 
-// #[derive(Accounts)]
-// pub struct Close<'info> {
-//      TODO: Implement Close accounts
-// }
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()], 
+        bump = vault_state.vault_bump,
+        
+    )]
+    pub vault: SystemAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+        close=user
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
 
-// impl<'info> Close<'info> {
-//     pub fn close(&mut self) -> Result<()> {
-//          TODO: Implement close
-//         Ok(())
-//     }
-// }
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        let lamports = self.vault.try_lamports()?;
+        let user = self.user.to_account_info();
+        let vault_state = self.vault_state.key();
+
+        let cpi_program = self.system_program.to_account_info();
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"vault",
+            vault_state.as_ref(),
+            &[self.vault_state.vault_bump],
+        ]];
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        transfer(cpi_ctx, lamports)?;
+
+        Ok(())
+    }
+}
 
 #[derive(InitSpace)]
 #[account]
 pub struct VaultState {
     pub vault_bump: u8,
     pub state_bump: u8,
+}
+
+#[error_code]
+pub enum Verror {
+    #[msg("Insufficient sol in vault")]
+    InsufficientSol,
 }
